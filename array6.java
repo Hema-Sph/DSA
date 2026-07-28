9cd789dad3bc8b00e69d0ff7faa7257db749670a
Hema Sphoorthi <hemasphoorthi15@gmail.com>
2026-07-28 22:25:35 +0530

    Name Search in an array
    Refactored name search logic to use a boolean flag for found status.

--- array6.java
+++ array6.java
@@ -1,20 +1,34 @@
+//Creating an array of names and Searching a given name in it
 import java.util.Scanner;
 public class array6 {
     public static void main(String[] args) {
        String[] names=new String[4];
        Scanner sc=new Scanner(System.in);
+       boolean found=false;
+        
        System.out.println("Enter names into the array: ");
+        
        for(int i=0;i<4;i++){
         names[i]=sc.nextLine();
        } 
        System.out.println("Enter the name to be searched: ");
        String a=sc.nextLine();
-
+    
        for(int i=0;i<4;i++){
-        if(names[i].equals(a)){
+            if(names[i].equals(a)){
+                found=true;
+                break;
+             }
+        }
+        if(found){
             System.out.println(a+" is in the array");
+        }
         
-    }System.out.println(a+" is not in the array");
-       }
+        else{
+             System.out.println(a+" is not in the array");
+        }
+       sc.close();
     }
 }
+
+
