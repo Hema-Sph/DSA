c71c837393126834411aa0175c430df26145f7b2
Hema Sphoorthi <hemasphoorthi15@gmail.com>
2026-07-28 20:21:02 +0530

    Sum of two 2D arrays
    Added comments for clarity on array operations.

--- array5.java
+++ array5.java
@@ -1,3 +1,4 @@
+//Sum of two 2D arrays
 import java.util.Scanner;
 public class array5 {
     public static void main(String[] args) {
@@ -5,7 +6,7 @@ public class array5 {
      int[][] mat1=new int[3][3];
      int[][] mat2=new int[3][3];
      int[][] sum=new int[3][3];
-     // 1st array
+    // 1st array
     System.out.println("Enter elements of the 1st array:");
     for(int i = 0; i < 3; i++){     
         for(int j = 0; j < 3; j++){         
@@ -19,12 +20,13 @@ public class array5 {
             mat2[i][j] =sc.nextInt();     
         } 
     }
+     //Summing up the 2 2D arrays together
      for(int i = 0; i < 3; i++){     
         for(int j = 0; j < 3; j++){         
             sum[i][j] =mat1[i][j]+mat2[i][j];     
         } 
     }  
-    
+    //Printing the Sum array
     for(int i = 0; i < 3; i++){     
         for(int j = 0; j < 3; j++){         
             System.out.print( sum[i][j]+" ");
